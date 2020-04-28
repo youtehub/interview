c1bd7f95732f1c896c0a3d1c7da9b643c1eaef3c
youtehub <3151626114@qq.com>
2020-04-28 18:53:51 +0800

    java基础知识

--- src/main/java/job/seek/javasort/Sort04.java
+++ src/main/java/job/seek/javasort/Sort04.java
@@ -18,7 +18,7 @@ import java.util.Arrays;
 public class Sort04 {
 
     public static void main(String[] args) {
-        int arr[] = {6, 5, 3, 2, 4};
+        int arr[] = {6, 5, 3, 2, 4,14};
         //6s
 //        List<Integer> list = new ArrayList<>();
 //        for (int i = 0; i < 9999; i++) {
