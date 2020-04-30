1ee3245be9b00a1889669ad9484154935fe7c542
youtehub <3151626114@qq.com>
2020-04-30 15:09:21 +0800

    java基础知识

---
+++ src/main/java/job/seek/java8/FunctionalInterface.java
@@ -0,0 +1,20 @@
+package job.seek.java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+/**
+ * 函数式接口
+ *
+ * @author yiyh
+ */
+public class FunctionalInterface {
+    public static void main(String[] args) {
+        BinaryOperator<Integer> money = (x,y) -> x+y;
+        System.out.println(money.apply(12,74));
+    }
+}
--- src/main/java/job/seek/java8/Lambda.java
+++ src/main/java/job/seek/java8/Lambda.java
@@ -4,7 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BinaryOperator;
 
-public class Java8 {
+/**
+ * Lambda表达式
+ *
+ * @author yiyh
+ */
+public class Lambda {
     public static void main(String[] args) {
         Runnable rum = () -> System.out.println("1. Lambda 表达式");
         rum.run();
@@ -13,7 +18,7 @@ public class Java8 {
             System.out.println("函数是接口方法");
             return x + y;
         };
-        System.out.println(cal.apply(33L,2L));
+        System.out.println(cal.apply(33L, 2L));
 
         List<String> list = new ArrayList<String>();
     }
