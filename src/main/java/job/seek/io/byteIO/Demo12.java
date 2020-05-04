1bbe338579b8f8deb1348e7d6ab5ae1010f928ff
youtehub <3151626114@qq.com>
2020-05-04 22:50:56 +0800

    java基础知识

--- src/main/java/job/seek/io/byteIO/Demo12.java
+++ src/main/java/job/seek/io/byteIO/Demo12.java
@@ -1,5 +1,7 @@
 package job.seek.io.byteIO;
 
+import org.junit.Test;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -26,5 +28,31 @@ public class Demo12 {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void testAdd() {
+        byte b;
+        b = 3;
+        int i = b * 3;
+        b = (byte) i;
+
+        float f1 = 100.00f;
+
+        Float F1 = new Float(f1);
+
+        double d1 = F1.doubleValue();//F1.doubleValue()为Float类的返回double值型的方法
+
+//        Integer num1 = 22;
+//        int num2 = 22;
+//        Long num3 = 22L;
+//        System.out.println(num1.equals(num3));
+//        System.out.println(num1 == num3);
+//        System.out.println(num3.equals(num2));
+
+        int num1 = 100;
+        int num2 = 200;
+        Long num3 = 300L;
+        System.out.println(num3.equals(num1 + num2)); //true
     }
 }
